46bc3fd2759002e75199b1991ce3ffc4699594a5
Yandi <karyana@yandi.me>
2025-08-14 05:51:30 +0700

    refactor: remove unused components and update deps
    Delete Card, Sidebar, and TimeLine components.
    Update dependencies and lockfile to latest versions.

--- src/components/card.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react"
-
-export const Card = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="flex aspect-square items-center justify-center rounded-md bg-background/50 shadow">
-      {children}
-    </div>
-  )
-}
